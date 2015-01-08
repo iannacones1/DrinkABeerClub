d0d46dd7edeaf53774509decaccc8af25b1b99e2
Steve Iannacone <iannacones1@gmail.com>
2015-01-08 07:55:57 -0500

    Update to run bash

--- dbcWeb.bash
+++ dbcWeb.bash
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+echo "Testing Internet Connection..."
+wget -q --tries=10 --timeout=0 --spider http://google.com > /dev/null
+if [[ $? -eq 0 ]]; then
+    echo "Online"
+else
+    echo "Offline"
+    exit 1
+fi
+
 #USERPAGE
 cd /home/pi/git/DrinkABeerClub
 
-./UserPage.rb && mv *.html /var/www/ && mv *.cor MapMaker/
+./UserPage.rb && mv *.html /var/www/
 
-cd /home/pi/git/DrinkABeerClub/MapMaker
+#MAPMAKER
+if [[ $? -eq 0 ]]; then
 
-./MapMaker.bash && mv *.png /var/www/ && mv *.cor backup/
+    mv *.cor MapMaker/
+
+    cd /home/pi/git/DrinkABeerClub/MapMaker
+
+    ./MapMaker.bash && mv *.png /var/www/
+
+    mv *.cor backup/
+
+fi
 
 #2014
 cd /home/pi/git/DrinkABeerClub
 
-./2014_50States.rb && mv table.html /var/www/table.html
+./2015_Styles.rb && mv table.html /var/www/table.html
